9c5e1c1c439094feba273585c8eb0beeef02e3e5
carlosaugustorodrigues <carlos.augusto01503@hotmail.com>
2024-11-05 08:28:52 -0300

    att

--- JOGODAMEMORIA/main.cpp
+++ JOGODAMEMORIA/main.cpp
@@ -9,7 +9,7 @@ const int linha = 4, coluna = 4, pares = (linha * coluna)/2;
 int auxiliarPares = pares, linhaAle, colunaAle, pontuacao = 0, x1, x2, y1, y2;
 char TABGAB[linha][coluna], TABSEC[linha][coluna], letra = 65;
 
-void preencherTabuleiros() {
+void inicializarTabuleiros() {
     srand(time(NULL));
 
     for (int i = 0; i < linha; i++) {
@@ -34,24 +34,43 @@ void preencherTabuleiros() {
 }
 
 void imprimirTabela() {
-     cout << "  ";
-        for (int i = 0; i < coluna; i++) {
-            cout << i << " ";
+    cout << "  ";
+    for (int i = 0; i < coluna; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < linha; i++) {
+        cout << i << " ";
+        for (int j = 0; j < coluna; j++) {
+            cout << TABSEC[i][j] << " ";
         }
         cout << endl;
+    }
+}
 
-        for (int i = 0; i < linha; i++) {
-            cout << i << " ";
-            for (int j = 0; j < coluna; j++) {
-                cout << TABSEC[i][j] << " ";
-            }
-            cout << endl;
-        }
+bool validarEntrada(int x, int y) {
+    if (x < 0 || x > (linha - 1) || y < 0 || y > (coluna - 1) || TABSEC[x][y] != '*') {
+        cout << "Coordenadas inválidas ou carta já revelada! Tente novamente" << endl;
+        return false;
+    }
+    return true;
+}
+
+bool verificarCartas(char carta1, char carta2) {
+    if (carta1 != carta2) {
+        cout << endl << "Cartas diferentes, tente novamente!" << endl;
+        return false;
+    } else {
+        pontuacao++;
+        cout << endl << "Parabéns, você encontrou " << pontuacao << " par(es) de cartas" << endl;
+        return true;
+    }
 }
 
 void jogo() {
     while (pontuacao < pares) {
-        cout << endl << "Pontuacao: " << pontuacao << endl << endl;
+        cout << endl << "Pontuação: " << pontuacao << endl << endl;
         
         imprimirTabela();
 
@@ -59,55 +78,39 @@ void jogo() {
             cout << endl << "Selecione a primeira carta, digite a linha e coluna: ";
             cin >> x1 >> y1;
             cout << endl;
-
-            if (TABSEC[x1][y1] != '*') {
-                cout << endl << "Carta ja revelada, escolha novamente!" << endl;
-            }
-
-        } while (TABSEC[x1][y1] != '*');
+        } while (!validarEntrada(x1, y1));
 
         TABSEC[x1][y1] = TABGAB[x1][y1];
-        
+
         imprimirTabela();
 
         do {
             cout << endl << "Selecione a segunda carta, digite a linha e coluna: ";
             cin >> x2 >> y2;
             cout << endl;
-
-            if (TABSEC[x2][y2] != '*') {
-                cout << endl << "Carta ja revelada, escolha novamente!" << endl;
-            }
-        } while (TABSEC[x2][y2] != '*');
+        } while (!validarEntrada(x2, y2));
 
         TABSEC[x2][y2] = TABGAB[x2][y2];
-        
-        imprimirTabela();
 
-        if (TABSEC[x1][y1] != TABSEC[x2][y2]) {
-            cout << endl << "Cartas diferentes, tente novamente!" << endl;
+        imprimirTabela();
+        
+        if (!verificarCartas(TABSEC[x1][y1], TABSEC[x2][y2])) {
             TABSEC[x1][y1] = '*';
             TABSEC[x2][y2] = '*';
-        } else {
-            pontuacao++;
-            cout << endl << "Parabens, voce encontrou " << pontuacao << " par de cartas" << endl;
-            
         }
     }
 
-    cout << endl << "Parabens, voce encontrou todos os pares!" << endl;                
+    cout << endl << "Parabéns, você encontrou todos os pares!" << endl;                
 }
 
 
 int main() {
 	
 	cout << "***************" << endl;
-	cout << "***************" << endl;
-	cout << "JOGO DA MEMORIA" << endl;
-	cout << "***************" << endl;
+	cout << "JOGO DA MEMÓRIA" << endl;
 	cout << "***************" << endl;
     
-    preencherTabuleiros();
+    inicializarTabuleiros();
     jogo();
 
     return 0;
